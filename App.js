e4bdc0759631f6cdc736e5b9621ff563365e1243
1pei0 <91524786+1pei0@users.noreply.github.com>
2022-06-08 00:09:42 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider} from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+import Navigation from './src/navigation';
+import React, { useEffect, getState } from "react";
+import {Provider } from "react-redux";
+import  store  from './src/redux/store';
 
-export default function App() {
+const App=() =>{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+     <Provider  store={store}>
+      <NativeBaseProvider>
+      <Navigation />  
+      </NativeBaseProvider>
+      </Provider>
+  </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
